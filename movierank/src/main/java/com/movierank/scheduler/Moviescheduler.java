d71c8814f28d7a77b40f93b78679a4ca29d44fa6
Mario-Hee <icon94@naver.com>
2020-04-17 15:42:30 +0900

    no message

---
+++ movierank/src/main/java/com/movierank/scheduler/Moviescheduler.java
@@ -0,0 +1,26 @@
+package com.movierank.scheduler;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.movierank.service.MovieService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class Moviescheduler {
+	
+	@Autowired
+	MovieService mService;
+	
+	@Scheduled(cron = "0 17 15 * * *")
+	public void movieConllect() throws IOException {
+		log.info(">>>>> Movie ticjetrank Collect");
+		mService.ticketRank();
+	}
+
+}
